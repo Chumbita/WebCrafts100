9ba5ad619f6a1c85272639dc46609df36281e2a7
Chumbita <chumbitalexis@gmail.com>
2024-01-16 17:55:25 -0300

    JS completo, falta mejorar y modificar el CSS para que no se desborden los elementos

--- project-6/app.js
+++ project-6/app.js
@@ -9,35 +9,42 @@ const $txtStep3 = document.getElementById("step-3");
 const $body = document.getElementById("body");
 const $section = document.getElementById("section");
 
-$btnN1.addEventListener("click", ()=>{
+
+function resetButtons() {
+    $btnN1.classList.remove("active");
+    $btnN2.classList.remove("active");
+    $btnN3.classList.remove("active");
+}
+
+$btnN1.addEventListener("click", () => {
     $txtStep1.style.display = "block";
     $txtStep2.style.display = "none";
     $txtStep3.style.display = "none";
     $body.style.backgroundColor = "#bc4c4c";
     $section.style.backgroundColor = "#c45c5c";
-    $btnN1.style.backgroundColor = "#c45c5c";
-    $btnN2.style.backgroundColor = "white";
-    $btnN3.style.backgroundColor = "white";
+
+    resetButtons();
+    $btnN1.classList.add("active");
 });
 
-$btnN2.addEventListener("click", ()=>{
+$btnN2.addEventListener("click", () => {
     $txtStep1.style.display = "none";
     $txtStep2.style.display = "block";
     $txtStep3.style.display = "none";
     $body.style.backgroundColor = "#3c848c";
     $section.style.backgroundColor = "#509494";
-    $btnN1.style.backgroundColor = "white";
-    $btnN2.style.backgroundColor = "#509494";
-    $btnN3.style.backgroundColor = "white";
+
+    resetButtons();
+    $btnN2.classList.add("active");
 });
 
-$btnN3.addEventListener("click", ()=>{
+$btnN3.addEventListener("click", () => {
     $txtStep1.style.display = "none";
     $txtStep2.style.display = "none";
     $txtStep3.style.display = "block";
     $body.style.backgroundColor = "#3c7494";
     $section.style.backgroundColor = "#4c7ca4";
-    $btnN1.style.backgroundColor = "white";
-    $btnN2.style.backgroundColor = "white";
-    $btnN3.style.backgroundColor = "#4c7ca4";
+
+    resetButtons();
+    $btnN3.classList.add("active");
 });
