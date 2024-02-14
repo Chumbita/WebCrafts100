3174e69bce6fb0bf313432ae1805501240b4226b
Chumbita <chumbitalexis@gmail.com>
2024-02-14 10:41:03 -0300

    Se corrigen algunos errores, se configuró la página para que sea responsive, se agregan animaciones y se mejora y optimiza el código para mejor estrucutra y posterior lecutra

--- project-012/app.js
+++ project-012/app.js
@@ -2,13 +2,17 @@ const $btnToggle = document.querySelectorAll(".btn");
 const $login = document.querySelector(".login-container");
 const $register = document.querySelector(".register-container");
 const $about = document.querySelector(".about-container");
+const $inputs = document.querySelectorAll(".inputs")
 
 console.log($btnToggle)
 
 $btnToggle.forEach(btn  => {
     btn.addEventListener("click", () => {
+        $about.classList.toggle("toggle");
         $login.classList.toggle("toggle");
         $register.classList.toggle("toggle");
-        $about.classList.toggle("toggle");
+        $inputs.forEach(input => {
+            input.value = "";
+        })
     })
 })
