e9401a4a8303ec71df7852cd50fa6db75d3e5dfc
Chumbita <chumbitalexis@gmail.com>
2024-03-01 09:34:26 -0300

    Se suben avances en las secciones y funcionalidades

--- project-015/app.js
+++ project-015/app.js
@@ -1,9 +1,9 @@
-const $infoBtns = document.querySelectorAll(".material-symbols-outlined.info-arrow");
+const $infoBtns = document.querySelectorAll(".circle");
 const $info = document.querySelector(".hero__info-text");
 
-console.log($infoBtn);
+console.log($infoBtns);
 
-$infoBtn.forEach(btn => {
+$infoBtns.forEach(btn => {
   btn.addEventListener("click", (e) => {
     console.log(e.target.nextSibling.nextSibling)
     e.target.nextSibling.nextSibling.classList.toggle("open");
