0db49f1c8881807ef53428f944dc16eaf5b172e0
Chumbita <chumbitalexis@gmail.com>
2024-03-15 09:22:08 -0300

    Se agrega un carrusel automático

--- project-016/script.js
+++ project-016/script.js
@@ -1,14 +1,16 @@
 const $btnLeft = document.querySelector(".btn-left");
 const $btnRight = document.querySelector(".btn-right");
-const $slider = document.getElementById("slider");
+const $slider = document.getElementById("slider-1");
 const $sliderSection = document.querySelectorAll(".slider-section");
+
+const $autoSlider = document.getElementById("auto-slider");
+const $sliders = document.querySelectorAll(".auto-slider-section");
 
 let operation = 0;
 let widthImg = 100 / $sliderSection.length;
 let sliderCount = 0;
 
-console.log($sliderSection.length);
-
+//CARUSEL 1
 $btnRight.addEventListener("click", moveToRight);
 $btnLeft.addEventListener("click", moveToLeft);
 
@@ -39,4 +41,25 @@ function moveToLeft() {
   }
   console.log(operation);
 }
+
+//CARUSEL 2
+let slide = 0; 
+let slideOperation = 0;
+let slideWidth = (100 / $sliders.length);
+
+let intervalID = setInterval(automaticCarusel, 5000);
+
+function automaticCarusel() {
+  slide ++;
+  if (slide > $sliders.length - 1) {
+    slide = 0;
+    slideOperation = 0;
+    $autoSlider.style.transform = `translateX(${slideOperation})`;
+  }
+  else {
+    slideOperation += slideWidth;
+    $autoSlider.style.transform = `translateX(-${slideOperation}%)`;
+  }
+}
+
 
